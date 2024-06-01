676ee6c03773d992c1ac1f1429f4e80f4ab6b25c
RyanWeigel32 <ryan.weigel32@gmail.com>
2024-06-01 11:38:40 -0500

    Changes to string in input function

--- Customer_Banking/customer_banking.py
+++ Customer_Banking/customer_banking.py
@@ -11,7 +11,7 @@ def main():
     # Prompt the user to set the savings balance, interest rate, and months for the savings account.
     savings_balance = int(input("What is your savings balance? "))
     savings_interest = float(input("What is the interest rate? "))
-    savings_maturity = int(input("How many months is this cd for? "))
+    savings_maturity = int(input("How many months for the savings account? "))
 
     # Call the create_savings_account function and pass the variables from the user.
     updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
